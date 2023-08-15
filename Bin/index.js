52a3f72227fd1d20b1b6dcc1c7bface660bb8020
Emanuel Leal <emanuel.s.leal@gmail.com>
2023-08-15 17:24:32 +0100

    teste

--- Bin/index.js
+++ Bin/index.js
@@ -1,35 +1,37 @@
 const { input, rawlist, select } = require("@inquirer/prompts");
 const todos = [];
+let iD = 0;
+
 function createTodo() {
   const todo = {};
 
   console.log("\n\nCreating a todo...");
   input({ message: "Enter the title: " }).then(function (title) {
     todo.title = title;
-
+    todo.id = iD;
     input({ message: "Enter the description: " }).then(function (description) {
       todo.description = description;
 
       input({ message: "Enter the SubTask: " }).then(function (subtask) {
         todo.subtask = subtask;
         //if()
-        //criar uma question para introduzir varias subtasks, 
-        //e perguntar se se quer criar nova ou sair 
+        //criar uma question para introduzir varias subtasks,
+        //e perguntar se se quer criar nova ou sair
 
         select({
           message: "Select a priority",
           choices: [
             {
               name: "Low",
-              value: "1",
+              value: "Low",
             },
             {
               name: "Medium",
-              value: "2",
+              value: "Medium",
             },
             {
               name: "High",
-              value: "3",
+              value: "High",
             },
           ],
         }).then(function (priority) {
@@ -42,20 +44,20 @@ function createTodo() {
             choices: [
               {
                 name: "0%",
-                value: "1",
+                value: "0%",
               },
               {
                 name: "25%",
-                value: "2",
+                value: "25%",
               },
               {
                 name: "50%",
-                value: "3",
+                value: "50%",
               },
               {
                 name: "100%",
-                value: "4",
-              }
+                value: "100%",
+              },
             ],
           }).then(function (status) {
             todo.status = status;
@@ -64,7 +66,7 @@ function createTodo() {
             todos.push(todo);
 
             console.log("\n");
-
+            iD++;
             renderMenu();
           });
         });
@@ -76,6 +78,7 @@ function createTodo() {
 function printTodos(list) {
   list.forEach(function (todo) {
     console.log("\n===============");
+    console.log(todo.id);
     console.log(todo.title);
     console.log(todo.description);
     console.log(todo.priority);
@@ -98,12 +101,29 @@ function sortByTitle() {
   renderMenu();
 }
 
+function sortByPrio() {
+  const order = ["High", "Medium", "Low"];
+  const todosSortedByPrio = todos.sort(
+    (a, b) => order.indexOf(a.priority) - order.indexOf(b.priority)
+  );
+  printTodos(todosSortedByPrio);
+  renderMenu();
+}
+
+function filterBiPrio(){
+
+
+  renderMenu();
+}
+
 function renderListMenu() {
   rawlist({
     message: "Listing all TodoList",
     choices: [
       { name: "List all", value: "all" },
       { name: "Sort by Title", value: "sortTitle" },
+      { name: "Sort by Priority", value: "sortPrio" },
+      { name: "Filter by Priority", value: "filterPrio" },
       { name: "Go back", value: "back" },
     ],
   }).then(function (option) {
@@ -113,6 +133,9 @@ function renderListMenu() {
         break;
       case "sortTitle":
         sortByTitle();
+        break;
+      case "sortPrio":
+        sortByPrio();
         break;
       case "back":
         renderMenu();
